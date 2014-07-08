34764d19fe087db85b5f37067528b282cab64802
Ian Morrison <ian@yola.com>
2014-07-08 10:04:41 -0700

    Fix spacing (2 spaces!)
    This isn't python!

--- test/xhr-helper-spec.js
+++ test/xhr-helper-spec.js
@@ -6,9 +6,9 @@ chai.should();
 describe('xhr helper', function() {
   it('returns a status code', function() {
     var status = {
-        xhr: {
-          status: 404
-        }
+      xhr: {
+        status: 404
+      }
     };
 
     xhrHelper.status(status).should.equal(404)
@@ -16,18 +16,18 @@ describe('xhr helper', function() {
 
   it('returns a parsed response object', function() {
     var response = {
-        xhr: {
-          response: '{"key": "value"}'
-        }
+      xhr: {
+        response: '{"key": "value"}'
+      }
     };
     xhrHelper.response(response).should.deep.equal({key: "value"});
   });
 
   it('returns response test', function() {
     var response = {
-        xhr: {
-          responseText: 'Happy Server'
-        }
+      xhr: {
+        responseText: 'Happy Server'
+      }
     };
     xhrHelper.responseText(response).should.equal('Happy Server');
   });
